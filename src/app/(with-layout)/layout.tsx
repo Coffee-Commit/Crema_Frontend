2912ba129a611e116c343bcacfb927f0b00ba280
조연경 <sig.yg5057@gmail.com>
2025-08-25 13:40:35 +0900

    style: pretendard 글꼴 적용

--- src/app/(with-layout)/layout.tsx
+++ src/app/(with-layout)/layout.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from 'react'
 import '@/styles/globals.css'
+import { pretendard } from '@/components/fonts'
 
 export default function RootLayout({
   children,
@@ -7,7 +8,10 @@ export default function RootLayout({
   children: ReactNode
 }) {
   return (
-    <html lang="ko">
+    <html
+      lang="ko"
+      className={pretendard.variable}
+    >
       <body>
         <main className="container h-full">{children}</main>
       </body>
